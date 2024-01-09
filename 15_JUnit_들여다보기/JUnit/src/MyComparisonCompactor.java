cd4d5f42f3c06105d8adf9103a5e68de8ca0b6ff
muyaaho <skanthsu715@gmail.com>
2024-01-09 21:21:52 +0900

    refactor: 부정문을 긍정문으로 변경

--- 15_JUnit_들여다보기/JUnit/src/MyComparisonCompactor.java
+++ 15_JUnit_들여다보기/JUnit/src/MyComparisonCompactor.java
@@ -1,7 +1,7 @@
 public class MyComparisonCompactor {
-    private int contextLength;
-    private String expected;
-    private String actual;
+    private final int contextLength;
+    private final String expected;
+    private final String actual;
     private int prefix;
     private int suffix;
 
@@ -12,19 +12,20 @@ public class MyComparisonCompactor {
     }
 
     public String compact(String msg) {
-        if (shouldNotCompact())
+        if (canBeCompacted()) {
+            findCommonPrefix();
+            findCommonSuffix();
+
+            String compactExpected = compactString(expected);
+            String compactActual = compactString(actual);
+            return Assert.format(msg, compactExpected, compactActual);
+        } else {
             return Assert.format(msg, expected, actual);
-
-        findCommonPrefix();
-        findCommonSuffix();
-
-        String compactExpected = compactString(expected);
-        String compactActual = compactString(actual);
-        return Assert.format(msg, compactExpected, compactActual);
+        }
     }
 
-    private boolean shouldNotCompact() {
-        return expected == null || actual == null || areStringEqual();
+    private boolean canBeCompacted() {
+        return expected != null && actual != null && !areStringEqual();
     }
 
     private boolean areStringEqual() {
@@ -33,14 +34,15 @@ public class MyComparisonCompactor {
 
     private String compactString(String s) {
         String result = "[" + s.substring(prefix, s.length() - suffix + 1) + "]";
-        if (prefix > 0)
+        if (prefix > 0) {
             result = (prefix > contextLength ? "..." : "") + expected.substring(Math.max(0, prefix - contextLength),
                     prefix) + result;
+        }
         if (suffix > 0) {
             int end = Math.min(expected.length() - suffix + 1 + contextLength, expected.length());
             result = result + (expected.substring(expected.length() - suffix + 1, end) + (
                     expected.length() - suffix + 1 < expected.length() - contextLength
-                    ? "..." : ""));
+                            ? "..." : ""));
         }
         return result;
     }
@@ -49,17 +51,19 @@ public class MyComparisonCompactor {
         prefix = 0;
         int end = Math.min(expected.length(), actual.length());
         for (; prefix < end; prefix++) {
-            if (expected.charAt(prefix) != actual.charAt(prefix))
+            if (expected.charAt(prefix) != actual.charAt(prefix)) {
                 break;
+            }
         }
     }
 
     private void findCommonSuffix() {
         int sfx1 = expected.length() - 1;
-        int sfx2 = actual.length() -1;
+        int sfx2 = actual.length() - 1;
         for (; sfx2 >= prefix && sfx1 >= prefix; sfx2--, sfx1--) {
-            if (expected.charAt(sfx1) != actual.charAt(sfx2))
+            if (expected.charAt(sfx1) != actual.charAt(sfx2)) {
                 break;
+            }
         }
 
         suffix = expected.length() - sfx1;
